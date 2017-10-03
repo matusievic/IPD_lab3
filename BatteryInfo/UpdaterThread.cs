931a5bd84a6bf531d45dc098798e38dc03e73e9e
Andrei Matusevich <>
2017-10-03 17:09:42 +0200

    Updater thread was added

---
+++ BatteryInfo/UpdaterThread.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BatteryInfo
+{
+    class UpdaterThread
+    {
+        private Thread Thread;
+        private bool _isInterrupted;
+        public bool IsInterrupted
+        {
+            get { return _isInterrupted; }
+            set { _isInterrupted = value; }
+        }
+        private BatteryInfoForm Form;
+        public UpdaterThread(BatteryInfoForm form)
+        {
+            Form = form;
+            Thread = new Thread(this.Updater);
+            _isInterrupted = false;
+        }
+
+        private void Updater()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Battery.UpdateData();
+                if (_isInterrupted)
+                {
+                    break;
+                }
+                Form.Invoke((MethodInvoker)delegate { Form.UpdateFormData(); });
+            }
+        }
+
+        public void Start()
+        {
+            Thread.Start();
+        }
+    }
+}
